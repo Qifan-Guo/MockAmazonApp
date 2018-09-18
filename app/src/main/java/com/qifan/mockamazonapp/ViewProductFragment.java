188581e6c489a8998d843830a3ccc7a4fac88635
Qifan Guo <qifanguo@gmail.com>
2018-09-18 16:21:23 -0400

    Initial Setup. Layout+Util Classes

---
+++ app/src/main/java/com/qifan/mockamazonapp/ViewProductFragment.java
@@ -0,0 +1,53 @@
+package com.qifan.mockamazonapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qifan.mockamazonapp.databinding.FragmentViewProductBinding;
+import com.qifan.mockamazonapp.util.Products;
+
+
+/**
+ * Created by User on 2/6/2018.
+ */
+
+public class ViewProductFragment extends Fragment {
+
+    private static final String TAG = "ViewProductFragment";
+
+    // Data binding
+    FragmentViewProductBinding mBinding;
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentViewProductBinding.inflate(inflater);
+        
+        Products products = new Products();
+        mBinding.setProduct(products.PRODUCTS[0]);
+    
+        mBinding.setQty(1);
+
+        return mBinding.getRoot();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/qifan/mockamazonapp/util/PreferenceKeys.java
@@ -0,0 +1,8 @@
+package com.qifan.mockamazonapp.util;
+
+
+public class PreferenceKeys {
+
+    public static final String shopping_cart = "shopping_cart";
+
+}
